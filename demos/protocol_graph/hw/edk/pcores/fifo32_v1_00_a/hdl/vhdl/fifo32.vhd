d83e210524f3a377846843e096b1b3889c30a10a
Stefan Kronig <stefan.kronig@gmail.com>
2013-06-25 00:33:44 +0200

    Adapted Generics' default values to make FIFO backwards compatible to legacy version. Made code more readable (comments / signal naming)

--- demos/protocol_graph/hw/edk/pcores/fifo32_v1_00_a/hdl/vhdl/fifo32.vhd
+++ demos/protocol_graph/hw/edk/pcores/fifo32_v1_00_a/hdl/vhdl/fifo32.vhd
@@ -7,27 +7,28 @@ use ieee.std_logic_unsigned.all;
 --use proc_common_v3_00_a.proc_common_pkg.all;
 
 entity fifo32 is
-	generic (
-		C_FIFO32_WORD_WIDTH       	:	integer	:= 32;	-- width of data words
-		C_FIFO32_DATA_SIGNAL_WIDTH	:	integer	:= 16;	-- width of "Fill" and "Rem" data signals
-		C_FIFO32_DEPTH            	:	integer := 16;
-		CLOG2_FIFO32_DEPTH        	:	integer := 4;
-		C_FIFO32_SAFE_READ_WRITE  	:	boolean := true -- enable "safe read" and "safe write"
+	generic (	-- Note: These default values were chosen to ensure backwards compatibility with the older version of the fifo32 entity (before June 2013). 
+	         	-- Do not change them unless you know what this implies for code which relies on the older version.
+	         	C_FIFO32_WORD_WIDTH         	:	integer	:= 32;  	-- width of data words
+	         	C_FIFO32_DEPTH              	:	integer	:= 16;  	-- depth (# elements) of the FIFO
+	         	CLOG2_FIFO32_DEPTH          	:	integer	:= 4;   	-- ceil(log2(depth)), i.e. #bits needed to represent the fill and remainer
+	         	C_FIFO32_CONTROLSIGNAL_WIDTH	:	integer	:= 16;  	-- width of "Fill" and "Rem" control signals. Must be > CLOG2...DEPTH
+	         	C_FIFO32_SAFE_READ_WRITE    	:	boolean	:= false	-- if enabled, the FIFO automatically prevents reading from it, if it is empty resp. writing if it is full. 
 	);
 	port (
 		Rst           	:	in	std_logic;
 		-- ..._M_...  	=	input of the FIFO.
 		-- ..._S_...  	=	output of the FIFO.
-		FIFO32_S_Clk  	:	in 	std_logic;                                              	-- clock and data signals
-		FIFO32_M_Clk  	:	in 	std_logic;                                              	
-		FIFO32_S_Data 	:	out	std_logic_vector(C_FIFO32_WORD_WIDTH-1 downto 0);       	
-		FIFO32_M_Data 	:	in 	std_logic_vector(C_FIFO32_WORD_WIDTH-1 downto 0);       	
-		FIFO32_S_Fill 	:	out	std_logic_vector(C_FIFO32_DATA_SIGNAL_WIDTH-1 downto 0);	-- # elements in the FIFO. 0 means FIFO is empty.
-		FIFO32_M_Rem  	:	out	std_logic_vector(C_FIFO32_DATA_SIGNAL_WIDTH-1 downto 0);	-- remaining free space. 0 means FIFO is full.
-		FIFO32_S_Full 	:	out	std_logic;                                              	-- FIFO full signal
-		FIFO32_M_Empty	:	out	std_logic;                                              	-- FIFO empty signal
-		FIFO32_S_Rd   	:	in 	std_logic;                                              	-- output data ready
-		FIFO32_M_Wr   	:	in 	std_logic                                               	-- write enabled
+		FIFO32_S_Clk  	:	in 	std_logic;                                                	-- clock and data signals
+		FIFO32_M_Clk  	:	in 	std_logic;                                                	
+		FIFO32_S_Data 	:	out	std_logic_vector(C_FIFO32_WORD_WIDTH-1 downto 0);         	
+		FIFO32_M_Data 	:	in 	std_logic_vector(C_FIFO32_WORD_WIDTH-1 downto 0);         	
+		FIFO32_S_Fill 	:	out	std_logic_vector(C_FIFO32_CONTROLSIGNAL_WIDTH-1 downto 0);	-- # data elements in the FIFO. 0 means FIFO is empty.
+		FIFO32_M_Rem  	:	out	std_logic_vector(C_FIFO32_CONTROLSIGNAL_WIDTH-1 downto 0);	-- remaining free space. 0 means FIFO is full.
+		FIFO32_S_Full 	:	out	std_logic;                                                	-- FIFO full signal
+		FIFO32_M_Empty	:	out	std_logic;                                                	-- FIFO empty signal
+		FIFO32_S_Rd   	:	in 	std_logic;                                                	-- output data ready
+		FIFO32_M_Wr   	:	in 	std_logic                                                 	-- write enabled
 	);
 end entity;
 
@@ -40,7 +41,7 @@ architecture implementation of fifo32 is
 	signal	fill      	: std_logic_vector(CLOG2_FIFO32_DEPTH-1 downto 0);
 	signal	full      	: std_logic;
 	signal	empty     	: std_logic;
-	signal	pad       	: std_logic_vector(C_FIFO32_DATA_SIGNAL_WIDTH-1 - CLOG2_FIFO32_DEPTH downto 0);
+	signal	pad       	: std_logic_vector(C_FIFO32_CONTROLSIGNAL_WIDTH-1 - CLOG2_FIFO32_DEPTH downto 0);
 	signal	safe_read 	: std_logic; 
 	signal	safe_write	: std_logic; 
 
@@ -58,7 +59,7 @@ begin
 	FIFO32_S_Full 	<= full;
 	FIFO32_M_Empty	<= empty;
 
-	--you can enable or disable this feature by setting the "C_FIFO32_SAFE_READ_WRITE" gereric.
+	-- you can enable or disable this feature by setting the "C_FIFO32_SAFE_READ_WRITE" gereric.
 	safe_read_write : process(FIFO32_S_Rd, FIFO32_M_Wr, empty, full)
 	begin
 		if C_FIFO32_SAFE_READ_WRITE = true then
